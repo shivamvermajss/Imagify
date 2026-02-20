52bb9278c5f9026f4e90ea6e8347e6c118ad543d
Shivam Verma <shivamvermacse2026@gmail.com>
2026-02-20 13:57:38 +0530

    Fix case sensitive import issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Result from './pages/Result'
 import BuyCredit from './pages/BuyCredit'
 import Navbar from './components/Navbar'
